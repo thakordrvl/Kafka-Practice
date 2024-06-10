21f737c1665a3a7eae90bcc1896eed5b84acebd0
Dhruvil <thakordrvl@gmail.com>
2024-06-10 11:10:04 +0530

    Moving stuff

---
+++ src/main/java/net/javaguides/springboot/controller/JsonMessageController.java
@@ -0,0 +1,25 @@
+package net.javaguides.springboot.controller;
+import net.javaguides.springboot.kafka.JsonKafkaProducer;
+import net.javaguides.springboot.payload.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class JsonMessageController {
+
+    private final JsonKafkaProducer jsonKafkaProducer;
+
+    public JsonMessageController(JsonKafkaProducer jsonKafkaProducer) {
+        this.jsonKafkaProducer = jsonKafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user){
+        jsonKafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json Message sent to kafka topic");
+    }
+}
---
+++ src/main/java/net/javaguides/springboot/payload/User.java
@@ -0,0 +1,46 @@
+package net.javaguides.springboot.payload;
+
+public class User {
+
+
+
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
